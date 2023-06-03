d3ff6ee2c0f9b48b6810b965205d866f39d67a07
PracticeEveryday <developerforus@gmail.com>
2023-06-03 22:57:19 +0900

    fix: deploy.yml 수정 4

--- scripts/deploy-before.sh
+++ scripts/deploy-before.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 echo "💡deploy before"
-if [ -d /home/ec2-user/13th-4team-backend ]; then
-    sudo rm -rf /home/ec2-user/13th-4team-backend
-fi
-sudo mkdir -vp /home/ec2-user/13th-4team-backend
+sudo chown -R ec2-user:ec2-user .
